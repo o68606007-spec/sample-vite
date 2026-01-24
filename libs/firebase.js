3ebc41a393d3ff17ac5fbaf84c85e15e97574917
OKblog <otsukikazuki@outlook.com>
2026-01-24 16:11:51 +0900

    firebase.js ソース変更

---
+++ libs/firebase.js
@@ -0,0 +1,24 @@
+// import firebase from "firebase/app";
+// import "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const config = {
+  apiKey: process.env.REACT_APP_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// firebase.initializeApp(config);
+
+// export default firebase;
+
+// 初期化
+const app = initializeApp(firebaseConfig);
+
+// Analytics 初期化（ブラウザ環境のみ）
+const analytics = getAnalytics(app);

25a853aecd3797803ac506a3c97c2b58f7b168c4
OKblog <otsukikazuki@outlook.com>
2026-01-24 16:41:37 +0900

    main firebase ソース変更

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App.jsx";
+
+import { analytics } from "../libs/firebase";
+
+// 本番環境のみ計測
+if (process.env.NODE_ENV === "production") {
+  analytics.analytics();
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);

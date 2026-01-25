2b80f5780bbe038406b81b9e1ae7a095e4b43e12
OKblog <otsukikazuki@outlook.com>
2026-01-25 12:05:02 +0900

    supabase記載変更 Vite用

---
+++ utils/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+// const SUPABASE_URL = process.env.VITE_SUPABASE_URL as string;
+// const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY as string;
+
+// console.log(SUPABASE_URL);
+// console.log(SUPABASE_ANON_KEY);
+
+export const supabase = createClient(
+  import.meta.env.VITE_PUBLIC_SUPABASE_URL,
+  import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
+);

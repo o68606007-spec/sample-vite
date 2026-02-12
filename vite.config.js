8e8821e4adf84d666a5b0dbad18e2835d1b825b7
o68606007-spec <otsukikazuki@outlook.com>
2026-02-12 23:06:02 +0900

    te4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import env from "vite-plugin-env-compatible";
+// import env from "vite-plugin-env-compatible";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), env({ prefix: "VITE", mountedPath: "import.meta.env" })],
-  base: "",
+  plugins: [react()],
+  // plugins: [react(), env({ prefix: "VITE", mountedPath: "import.meta.env" })],
+  // base: "",
 
   // build: {
   //   rollupOptions: {
